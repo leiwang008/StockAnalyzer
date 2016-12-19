7f91c24be109bd335fb400cbd12c03ec21a68e7f
sbjlwa <lei.wang@sas.com>
2016-12-19 09:54:09 +0800

    Put the proxy server and port string into the properties file.

--- src/CalendarTest.java
+++ src/CalendarTest.java
@@ -2,12 +2,15 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Locale;
+
+import com.lei.GetText;
 
 
 public class CalendarTest {
     public static void main(String[] args) {
     	HttpURLConnection con =null;
-    	
+
     	try{
     		ProxyDetector pd = ProxyDetector.getInstance();
     		if(pd.proxyDetected()){
@@ -15,25 +18,29 @@ public class CalendarTest {
     			System.setProperty("http.proxyHost",pd.getHostname());
   	  			System.setProperty("http.proxyPort",String.valueOf(pd.getPort()));
     		}
-    		
 
-			System.setProperty("http.proxyHost","inetgw.unx.sas.com");
-	  		System.setProperty("http.proxyPort","80");
-	  			
+    		GetText bundle = new GetText(GetText.DEFAULT_BUNDLE_NAME, Locale.ENGLISH);
+
+    		String proxyServer = bundle.text("key.http.proxy.server");
+    		String proxyPort = bundle.text("key.http.proxy.port");
+
+			System.setProperty("http.proxyHost", proxyServer);
+	  		System.setProperty("http.proxyPort", proxyPort);
+
     		String urlstring = "http://sww.sas.com";
-    		
+
     		urlstring = "http://www.google.com";
-    		
+
   	  		URL feedUrl = new URL(urlstring);
-  	  	
-  	  		
+
+
   	  		con = (HttpURLConnection) feedUrl.openConnection();
-  	  		
+
   	  		//If the url need user and password
-//  	  		sun.misc.BASE64Encoder encoder = new sun.misc.BASE64Encoder();  
+//  	  		sun.misc.BASE64Encoder encoder = new sun.misc.BASE64Encoder();
 //  	  		String encodedUserPwd = encoder.encode("sbjlwa:Xudongsun3685".getBytes());
 //  	  		con.setRequestProperty("Authorization", "Basic " + encodedUserPwd);
-  	  		
+
   	  		BufferedReader input = new BufferedReader(new InputStreamReader(con.getInputStream()));
   			StringBuffer htmlContent = new StringBuffer();
   			String tmp = input.readLine();
@@ -41,9 +48,9 @@ public class CalendarTest {
   				htmlContent.append(tmp);
   				tmp = input.readLine();
   			}
-//    		
+//
   			System.out.println(htmlContent.toString());
-  			
+
 //  	  		CalendarService myService = new CalendarService("exampleCo-exampleApp-1.0");
 //    		//myService.setUserCredentials("root@gmail.com", "pa$$word");
 //			myService.setUserCredentials("leiwang008@yahoo.com.cn", "Xudongsun3685");
@@ -58,12 +65,12 @@ public class CalendarTest {
 //				CalendarEntry entry = resultFeed.getEntries().get(i);
 //				System.out.println("\t" + entry.getTitle().getPlainText());
 //			}
-			
-			
+
+
     	}catch(Exception e){
     		e.printStackTrace();
     	}
-    
+
     }
 
 }
